cb3b0ede17bb701fe40ccc68b7256f8f5da4e6b4
hakobe932 <hakobe@gmail.com>
2009-05-17 21:54:15 +0900

    pmsetuped

---
+++ lib/Pig.pm
@@ -0,0 +1,35 @@
+package Pig;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Pig -
+
+=head1 SYNOPSIS
+
+  use Pig;
+
+=head1 DESCRIPTION
+
+Pig is
+
+=head1 AUTHOR
+
+Yohei Fushii E<lt>hakobe@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
