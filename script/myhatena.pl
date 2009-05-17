ad137242f85002fa742423f68b8201f404ab2c74
hakobe932 <hakobe@gmail.com>
2009-05-17 22:50:16 +0900

    設定をわかりやすく

--- script/myhatena.pl
+++ script/myhatena.pl
@@ -4,7 +4,10 @@ use Pig;
 use Pig::Service::MyHatena;
 
 my $pig = Pig->new(
-    service => Pig::Service::MyHatena->new,
+    service => Pig::Service::MyHatena->new( 
+        interval => 300,  # 5分毎にチェック
+    ),
+    config => { port => 16667 },
 );
 $pig->run;
 
