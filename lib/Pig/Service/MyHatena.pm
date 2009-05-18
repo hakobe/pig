d9f0edede0aeb7c94bddcba31f9e046b84882ef1
hakobe932 <hakobe@gmail.com>
2009-05-18 20:44:53 +0900

    Service::MyHatena を Service::FeedReader ベースに

--- lib/Pig/Service/MyHatena.pm
+++ lib/Pig/Service/MyHatena.pm
@@ -2,132 +2,72 @@ package Pig::Service::MyHatena;
 use strict;
 use warnings; 
 
+use Moose;
+
 use XML::Feed;
 use URI;
 use DateTime;
 use Time::HiRes qw(sleep);
 
-use Moose;
-# TODO Pig::Service を Role にする
+use Pig::Service::FeedReader::Channel;
+use Pig::Service::FeedReader::Feed;
 
-has interval => (
-    is => 'ro',
-    default => sub {
-        60 * 30; # 30分
-    }
-);
+use Pig::Service::FeedReader;
+extends qw(Pig::Service::FeedReader);
 
 has hatena_id => (
     is => 'ro',
 );
 
-has last_updates => (
-    is => 'rw',
-    isa => 'HashRef',
-    default => sub { +{} },
-);
-
-has hatena_users => (
-    is => 'rw',
-    isa => 'HashRef',
-    default => sub { +{} },
-);
-
-sub fix_last_update_for {
-    my ($self, $user) = @_;
-    $self->last_updates->{$user} = DateTime->now(time_zone => 'Asia/Tokyo');
-}
-
-sub on_start { }
-
 sub on_check {
     my ($self, $pig) = @_;
 
-    $self->check_antenna($pig);
-    $self->check_hatena_user($pig);
-}
+    for my $channel ( values %{ $self->channels } ) {
+        next unless $channel->is_active;
+        my ($bot_name) = $channel->name =~ m/^\#(.*)$/xms;
 
-# TODO いまはほとんどcheck_hatena_userと一緒だけど author の扱い方で結構かわってきそうだ
-sub check_antenna {
-    my ($self, $pig) = @_;
-    return unless $pig->ircd->state_chan_exists('#antenna');
-    warn "checking antenna";
-
-    my $rss_uri = URI->new(sprintf('http://www.hatena.ne.jp/%s/antenna.rss', $self->hatena_id));
-
-    # TODO If-Modifed-Since をみて抜けたりする
-    my $feed = XML::Feed->parse($rss_uri);
-    sleep 1; # 適度にまつ
-
-    if (!$feed) {
-        warn "antenna: fetching rss is failed";
-        return;
-    }
-        
-    my $has_new = 0;
-    for my $entry (reverse($feed->entries)) {
-        next if $self->last_updates->{antenna} 
-                && $entry->issued < $self->last_updates->{antenna};
-
-        $has_new = 1;
-        # TODO メッセージフォーマットをconfigで指定できるよう
-        my $message = sprintf("%s: %s %s", $entry->author, $entry->title, $entry->link);
-        # TODO authorが発言できるとなお良い => anntenaのユーザのみを管理する必要
-        $pig->privmsg( 'antenna', "#antenna", $message );
-    }
-    $self->fix_last_update_for('antenna') if $has_new;
-}
-
-sub check_hatena_user {
-    my ($self, $pig) = @_;
-    warn "checking: " . (join ", ", keys %{ $self->hatena_users }) if keys %{ $self->hatena_users };
-
-    for my $hatena_user (keys %{ $self->hatena_users }) {
-        my $rss_uri = URI->new(sprintf('http://www.hatena.ne.jp/%s/activities.rss', $hatena_user))  ;
-
-        # TODO If-Modifed-Since をみて抜けたりする
-        my $feed = XML::Feed->parse($rss_uri);
-        sleep 1; # 適度にまつ
-
-        if (!$feed) {
-            warn "$hatena_user fetching rss is failed";
-            next;
+        for my $feed (@{ $channel->feeds }) {
+            $feed->each_new_entry( sub { 
+                my $entry = shift;
+                warn $entry->title; 
+                my $message = $bot_name eq 'antenna' ?
+                    sprintf("%s %s", ($entry->title || '[no title]'), ($entry->link || '[no url]')) :
+                    sprintf("%s: %s %s", $entry->author, $entry->title, $entry->link)               ;
+                $pig->privmsg( $bot_name, $channel->name, $message );
+            });
         }
-        
-        my $has_new = 0;
-        for my $entry (reverse($feed->entries)) {
-            next if $self->last_updates->{$hatena_user} 
-                 && $entry->issued < $self->last_updates->{$hatena_user};
-
-            $has_new = 1;
-            # TODO: メッセージフォーマットをconfigで指定できるよう
-            my $message = sprintf("%s %s", ($entry->title || '[no title]'), ($entry->link || '[no url]'));
-            $pig->privmsg( $hatena_user, "#$hatena_user", $message );
-        }
-        $self->fix_last_update_for($hatena_user) if $has_new;
     }
 }
-
-# TODO このへんのantennaの扱いをまともに
 
 sub on_ircd_join {
-    my ($self, $pig, $nick, $channel) = @_;
-    my ($hatena_user) = $channel =~ m/^\#(.*)$/xms;
-    return if $nick eq 'antenna';
-    return if $self->hatena_users->{$nick};
+    my ($self, $pig, $nick, $channel_name) = @_;
+    return if $self->channels->{$channel_name};
 
-    $pig->join($hatena_user, "#$hatena_user");
-    $self->hatena_users->{$hatena_user} = 1 if $hatena_user ne 'antenna';
+    my ($bot_name) = $channel_name =~ m/^\#(.*)$/xms;
+
+    my $feed_uri = $bot_name eq 'antenna' ?
+        sprintf('http://www.hatena.ne.jp/%s/antenna.rss', $self->hatena_id) :
+        sprintf('http://www.hatena.ne.jp/%s/activities.rss', $bot_name)  ;
+
+    my $channel = Pig::Service::FeedReader::Channel->new(
+        name => $channel_name,
+        feeds => [
+            Pig::Service::FeedReader::Feed->new(
+                uri => URI->new($feed_uri),
+            ),
+        ]
+    );
+    $channel->activate;
+    $self->channels->{$channel_name} = $channel;
+    $pig->join($bot_name, $channel_name);
 }
 
 sub on_ircd_part {
-    my ($self, $pig, $nick, $channel) = @_;
-    my ($hatena_user) = $channel =~ m/^\#(.*)$/xms;
-    return if $nick eq 'antenna';
-    return if $self->hatena_users->{$nick};
+    my ($self, $pig, $nick, $channel_name) = @_;
+    my ($bot_name) = $channel_name =~ m/^\#(.*)$/xms;
 
-    $pig->part($hatena_user, "#$hatena_user");
-    delete $self->hatena_users->{$hatena_user} if $hatena_user ne 'antenna';
+    $self->channels->{$channel_name}->deactivate;
+    $pig->part($bot_name, $channel_name);
 }
 
 __PACKAGE__->meta->make_immutable;
