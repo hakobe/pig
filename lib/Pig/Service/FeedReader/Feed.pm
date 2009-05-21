e188b782a1dc1bd8e9e0006fea476b6c92fd04a7
hakobe932 <hakobe@gmail.com>
2009-05-21 20:29:09 +0900

    Gmailの変なAtomフィードに対応 && XML::Atomがエンコードしすぎる問題

--- lib/Pig/Service/FeedReader/Feed.pm
+++ lib/Pig/Service/FeedReader/Feed.pm
@@ -3,6 +3,8 @@ use strict;
 use warnings;
 use Moose;
 use XML::Feed;
+use XML::Atom;
+$XML::Atom::ForceUnicode = 1; # FIXME こうしないと日本語がばける…
 use URI;
 use DateTime;
 use Time::HiRes qw(sleep);
@@ -36,13 +38,43 @@ sub each_new_entry {
     
     my $has_new = 0;
     for my $entry (reverse($xml_feed->entries)) {
-
+        # FIXME Gmailのフィードがhoursが24以上の日付を返してくるため
+        no warnings 'redefine';
+        local *XML::Feed::Entry::Format::Atom::iso2dt = \&iso2dt;
         next if $self->last_update && $entry->issued < $self->last_update;
         $has_new = 1;
         $code->($entry);
     }
     $self->fix_last_update if $has_new;
 }
+
+# FIXME from XML::Atom::Util
+sub iso2dt {
+    my($iso) = @_;
+    return unless $iso =~ /^(\d{4})(?:-?(\d{2})(?:-?(\d\d?)(?:T(\d{2}):(\d{2}):(\d{2})(?:\.\d+)?(?:Z|([+-]\d{2}:\d{2}))?)?)?)?/;
+    my($y, $mo, $d, $h, $m, $s, $zone) =
+        ($1, $2 || 1, $3 || 1, $4 || 0, $5 || 0, $6 || 0, $7);
+    warn 'mogeeee';
+    if ($h > 23) {
+        $h = $h % 24;
+        $d += 1;
+    }
+    require DateTime;
+    my $dt = DateTime->new(
+               year => $y,
+               month => $mo,
+               day => $d,
+               hour => $h,
+               minute => $m,
+               second => $s,
+               time_zone => 'UTC',
+    );  
+    if ($zone && $zone ne 'Z') {
+        my $seconds = DateTime::TimeZone::offset_as_seconds($zone);
+        $dt->subtract(seconds => $seconds);
+    }
+    $dt;
+} 
 
 
 __PACKAGE__->meta->make_immutable;
